a64b6449d6ead658a50d7fa924e60f99b27b4a15
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2024-02-16 21:56:09 +0500

    Update decoder.py

--- Model/decoder.py
+++ Model/decoder.py
@@ -7,7 +7,7 @@ class VAE_Decoder(nn.Sequential): ## each module will use to reduce the dimensio
     def __init__(self):
         super.__init__(
             #(Batch_size,Channel, Height, Width) -> (Batch_size, 128, Height, Width) output will in same size because we added the padding
-            nn.Conv2d(3,128,kernel_size=2,padding=1),
+            nn.Conv2d(3,128,kernel_size=3,padding=1),
 
             #(Batch_size,128, Height, Width) -> (Batch_size, 128, Height, Width)
             VAE_ResidualBlock(128,128),# this indicates how many input and output channel we have #combination of both normalization and convolutions
