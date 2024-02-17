8c55ed3f258803ed5c1135060596b47bfed2a473
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2024-02-17 18:40:06 +0500

    Update decoder.py

--- Model/decoder.py
+++ Model/decoder.py
@@ -20,12 +20,37 @@ class VAE_Decoder(nn.Sequential): ## each module will use to reduce the dimensio
             #(Batch_size,128, Height, Width) -> (Batch_size, 128, Height/2, Width/2)
             nn.Conv2d(128,128,kernel_size=3,padding=0, stride=2),  #   will reduce the size of the image by 2
 
-            #(Batch_size,128, Height, Width) -> (Batch_size, 128, Height/2, Width/2)
+            #(Batch_size,128, Height, Width) -> (Batch_size, 256, Height/2, Width/2)
             VAE_ResidualBlock(128,256),
 
 
-            #(Batch_size,256, Height, Width) -> (Batch_size, 128, Height/2, Width/2)
+            #(Batch_size,256, Height/4, Width/4) -> (Batch_size, 256, Height/2, Width/2)
             VAE_ResidualBlock(256,256),
+
+            #(Batch_size,256, Height, Width) -> (Batch_size, 256, Height/4, Width/4)
+            nn.Conv2d(256,256,kernel_size=3,padding=0, stride=2),  
+
+            #(Batch_size,256, Height/4, Width/4) -> (Batch_size, 512, Height/4, Width/4)
+            VAE_ResidualBlock(256,512),
+
+            #(Batch_size,256, Height/4, Width/4) -> (Batch_size, 512, Height/4, Width/4)
+            VAE_ResidualBlock(512,512),
+
+            #(Batch_size,512, Height/4, Width/4) -> (Batch_size, 512, Height/8, Width/8)
+            nn.Conv2d(512,512,kernel_size=3,padding=0, stride=2), 
+
+            VAE_ResidualBlock(512,512),
+
+
+            VAE_ResidualBlock(512,512),
+
+
+            VAE_ResidualBlock(512,512),
+
+
+
+
+
 
 
         )
