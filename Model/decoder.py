0ff1075a709ad50f34c2c1a771a7e3d3cbd29477
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2024-02-19 18:34:44 +0500

    Update decoder.py

--- Model/decoder.py
+++ Model/decoder.py
@@ -96,7 +96,55 @@ class Decoder (nn.Sequential):
             VAE_ResidualBlock(512,512),
 
 
+            #(Batch_size,412,Height/8,Width/8) -> (Batch_size,512,Height/4,Width/4)
+            nn.Upsample(scale_factor=2,),   #This will increase the size of the image by 2 times
+
+            nn.Conv2d(512,512,kernel_size=3,padding=0),
+
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+
+            #(Batch_size,512,Height/4,Width/4) -> (Batch_size,256,Height/2,Width/2)
+            nn.Upsample(scale_factor=2,),   #This will increase the size of the image by 2 times
+
+            nn.Conv2d(512,512,kernel_size=3,padding=0),
+
+            VAE_ResidualBlock(512,256),
+            VAE_ResidualBlock(256,256),
+            VAE_ResidualBlock(256,256),
+            #(Batch_size,256,Height/2,Width/2) -> (Batch_size,256,Height,Width)
+            nn.Upsample(scale_factor=2,),   #This will increase the size of the image by 2 times
+
+
+            nn.Conv2d(256.256,kernel_size=3,padding=0),
+
+
+            VAE_ResidualBlock(256,128),
+            VAE_ResidualBlock(128,128),
+            VAE_ResidualBlock(128,128),
+
+            nn.GroupNorm(32,128) ,# this will divide 128 into groups of 32 
+            nn.SiLU(),
+
+            #(Batch_size,128,Height,Width) -> (Batch_size,3(RGB),Height,Width)
+            nn.Conv2d(128,3,kernel_size=3,padding=1), 
+
         )
+
+
+def forward(self,x: torch.Tensor) -> torch.Tensor:
+    # x:(Batch_size,4,Height/8,Width/8) input is latent
+
+    x/= 0.18215
+
+    for module in self:
+        x= module(x)
+
+
+    #(Batch_size,3,Height,Width)
+    return x
+
 
 
 
